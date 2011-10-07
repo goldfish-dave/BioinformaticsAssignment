a28e8a22763231e4ed114c21d85961f11be96f3f
dave <dwgoldfish5@gmail.com>
2011-10-07 16:00:36 +1100

    up until now I've been trying to implement bruteForce median search. This was wrong, as bfms is an imperative algorithm by nature and I was trying to implement is as a functional tree search. I've now started implementing simple median search like I should have been to begin with. I now have a (non-working) implementation and have started on implementing searchTree as a top level function.

--- Code/MedianSearch.hs
+++ Code/MedianSearch.hs
@@ -7,6 +7,10 @@ import Data.Tree
 
 infinity = 1000 :: Int
 
+type BestWord = ([NukeTide], Int) -- (BestWord, BestDistance)
+
+searchTree = undefined
+
 bfMedSerch :: DNA -> Int -> [NukeTide]
 bfMedSerch dna l = fst $ ms tree ([],infinity)
 	where
@@ -36,3 +40,16 @@ bfMedSerch dna l = fst $ ms tree ([],infinity)
 		seed (nk, 0) = (nk, [])
 		seed (nk, i) = (nk, map (\x -> nk ++ [x]) [A,T,C,G] `zip` [i-1,i-1..])
 
+simpMedSerch :: DNA -> Int -> [NukeTide]
+-- Takes an n x t array of nucleotides and a length l
+-- and outputs the most likely motif of length l
+-- NB: This function does not implement branch and bounding
+simpMedSerch = undefined
+	where
+		ms :: Tree [NukeTide] -> BestWord -> BestWord
+		ms (Node x []) b@(bw,bd)       -- Leaf node
+			| True = undefined         -- x is better than b
+			| otherwise = undefined    -- b ix better than x
+		ms (Node _ xs) b@(bw,bd) = foldr (\node currBest -> bestOf (ms node currBest) currBest) b xs
+		-- The above line performs depth-first ms over all the children nodes,
+		-- returning the best BestWord
