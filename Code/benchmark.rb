62739002ca4c064dc4d7c1b6248e875a726cfc23
Wilcox; David <Wilcox; David dwil5124@ug.it.usyd.edu.au>
2011-10-14 23:05:52 +1100

    benchmark.rb stuff

--- Code/benchmark.rb
+++ Code/benchmark.rb
@@ -11,19 +11,14 @@ class Array
 	end
 end
 
-run_main = lambda do |forks,algorithm,length,file,display,cores|
-	if display
-		d = "-d"
-	else
-		d = ""
-	end
+def run_test(forks,algorithm,length,file,cores)
 	Benchmark.measure do
-		`./main -f #{(forks*cores).to_s} -l #{length.to_s} -o #{file.to_s} -a #{algorithm.to_s} #{d} +RTS -N#{cores.to_s}`
+		`./main -f #{(forks*cores).to_s} -l #{length.to_s} -o #{file.to_s} -a #{algorithm.to_s} +RTS -N#{cores.to_s}`
 	end
 end
 
 
-def test1(n=1,l=8,file)
+def testA(n,l,file)
 # in this test we compare the different algorithms on 1 core
 #   for the algorithms which can run in multiple forks we test 
 #   them on [1,2,4,8] forks
@@ -36,13 +31,13 @@ def test1(n=1,l=8,file)
 	algorithms.each do |a|
 		forks.each do |f|
 			results = []
-			n.times { results << test.call(f,a,l,file,false,1).real }
+			n.times { results << run_test(f,a,l,file,1).real }
 			puts "#{a},#{f},1,#{results.average},#{results.stdev}"
 		end
 	end
 end
 
-def main
-	file = "Data/aps_ref_Acyr_2.0_chrMT.fa"
-	test1(10,8,file)
-end
+## main ##
+#
+file = "Data/aps_ref_Acyr_2.0_chrMT.fa"
+testA(10,8,file)
